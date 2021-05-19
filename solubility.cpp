4c8b62a5d3c6c07838a0c85a43490847645450d9
Rahul <rnkr99@gmail.com>
2021-05-19 22:00:26 +0530

    Updated test case variable

--- solubility.cpp
+++ solubility.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int T;
-    cin>>T;
-    while(T--){
+    int t;
+    cin>>t;
+    while(t--){
         int X,A,B;
         cin>>X>>A>>B;
         int total=A+(100-X)*B;
